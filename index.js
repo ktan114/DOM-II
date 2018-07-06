a32899a36e9c07d52de2428355b6db5377f18e1d
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-07-06 18:34:26 -0400

    Structured code and removed unnecessary code

--- index.js
+++ index.js
@@ -1,49 +1,73 @@
-// elements in block
+// Selectors
+
 const red = document.querySelector('.block--red');
 const blue = document.querySelector('.block--blue');
 const green = document.querySelector('.block--green');
 const pink = document.querySelector('.block--pink');
 const gray = document.querySelector('.block--gray');
 
-// rockets
-    // When a block is clicked, it should go to the top of the stack
-    // holds current value of flex order at the time of a 'click' event
-    // let order = 0;
-    // event
-    // const move = () => {
-        // decrement order: (-) orders will move to the top of the main-axis
-        // default order is 0 for all blocks
-    // 	order--;
-    // 	event.target.style.order = order.toString();
-    // }
+// Rockets
 
-    // event listeners
-    // red.addEventListener('click', move);
-    // blue.addEventListener('click', move);
-    // green.addEventListener('click', move);
-    // pink.addEventListener('click', move);
-    // gray.addEventListener('click', move);
+/* 
+    When a block is clicked, it should go to the top of the stack
+    holds current value of flex order at the time of a 'click' event
+*/
+
+// let order = 0;
+    
+/* 
+    decrement order: (-) orders will move to the top of the main-axis
+    default order is 0 for all blocks
+    order is global so when it is clicked it will decrement
+    as it decrements, order is then assigned to the style.order of another block 
+*/
+
+// const move = (event) => {
+//     order--;
+//     event.target.style.order = order.toString(); 
+// }
+
+/*
+    Event Listeners 
+*/
+
+// red.addEventListener('click', move);
+// blue.addEventListener('click', move);
+// green.addEventListener('click', move);
+// pink.addEventListener('click', move);
+// gray.addEventListener('click', move);
 
 
-// travelers
-const block = document.getElementsByClassName('block');
+// Travelers
 
-// let marginPX = 10;
+/*
+    Event Callbacks
+*/
 
 const moveRight = (event) => {
 	let marginPX = 10;
-    window.setInterval( function() {
-    	// when margin is smaller than 1000 [1 screen length]
+    window.setInterval(function() {
         if (marginPX < 300) {
             marginPX++; 
             event.target.style.marginLeft = marginPX + 'px';
-        }
-        // margin reaches 100px
-        // else {
-        // 	clearInterval(moveRight);
-        // }
+        } 
     }, 5);
 }
+
+const moveBack = (event) => {
+    let marginPX = 300;
+	window.setInterval(function() {
+        event.stopImmediatePropagation();
+		if (marginPX > 10) {
+            marginPX--;
+			event.target.style.marginLeft = marginPX + 'px';
+		} 
+	}, 5);
+}
+
+/* 
+    Add Event Listeners
+*/
 
 red.addEventListener('mousedown', moveRight);
 blue.addEventListener('mousedown', moveRight);
@@ -51,27 +75,8 @@ green.addEventListener('mousedown', moveRight);
 pink.addEventListener('mousedown', moveRight);
 gray.addEventListener('mousedown', moveRight);
 
-// clearInterval(moveRight);
-
-const moveBack = (event) => {
-	let marginPX = 300;
-	window.setInterval(function() {
-		event.stopImmediatePropagation();
-		clearInterval(moveRight);
-		if (marginPX > 10) {
-			marginPX--;
-			event.target.style.marginLeft = marginPX + 'px';
-		}
-		else {
-			clearInterval(moveBack);
-		}
-	}, 5);
-}
-
 red.addEventListener('mouseup', moveBack);
 blue.addEventListener('mouseup', moveBack);
 green.addEventListener('mouseup', moveBack);
 pink.addEventListener('mouseup', moveBack);
 gray.addEventListener('mouseup', moveBack);
-
-
